3d53957b2982edb75aa45e88bc81509238950167
chsakell <chsakell@gmail.com>
2023-07-06 13:50:50 +0300

    Add project files.

---
+++ DatePickerApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace DatePickerApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ DatePickerApp/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace DatePickerApp;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+
+    private void DatePicker_DateSelected(object sender, DateChangedEventArgs e)
+    {
+
+    }
+}
+
